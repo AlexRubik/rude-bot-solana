3b179b0fe2d59854303ad0bcda589bd6eac9c863
dawg <dewittat@g.cofc.edu>
2024-06-09 10:20:41 -0400

    valid sol pair tokens

--- getTrendingTokens/main.py
+++ getTrendingTokens/main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Make the GET request
 url = "https://api.geckoterminal.com/api/v2/networks/solana/trending_pools"
@@ -23,6 +24,35 @@ if response.status_code == 200:
     # print the length of the array
     print(len(base_token_strings))
 
-    print("Successfully fetched data from the API")
+    print("Successfully fetched data from coingecko API")
+    
+    # Iterate over each base token string and make the GET request
+    jup_url = "https://quote-api.jup.ag/v6"
+    tokens_with_context_slot = []
+    for base_token in base_token_strings:
+        quote_url = f"{jup_url}/quote?inputMint=So11111111111111111111111111111111111111112&outputMint={base_token}&amount=1000000000&slippageBps=5&onlyDirectRoutes=false&asLegacyTransaction=false&maxAccounts=28"
+        quote_response = requests.get(quote_url)
+
+        if quote_response.status_code == 200:
+            response_json = quote_response.json()
+
+            # Check if the response has a contextSlot field
+            if "contextSlot" in response_json:
+                tokens_with_context_slot.append(base_token)
+                # has context slot so print the token
+                print(f"Valid SOL Pair token: {base_token}")
+
+            print("-" * 40)
+        else:
+            print(f"Invalid SOL Pair token {base_token}")
+
+        # Delay for 1 second
+        time.sleep(1)
+
+    # Print the tokens with contextSlot
+    print("Valid SOL Pair Tokens:")
+    print(tokens_with_context_slot)
+        
+
 else:
     print("Failed to fetch data from the API")
