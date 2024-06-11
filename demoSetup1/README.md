6b494c431db77c61fcc0fa86088896941e90e8e9
dawg <dewittat@g.cofc.edu>
2024-06-10 23:16:55 -0400

    demo

---
+++ demoSetup1/README.md
@@ -0,0 +1,19 @@
+- [QuickNode Scale Plan](https://www.quicknode.com/?via=rudearb) for reading (RPC_URL in .env and --rpc-url in jup api startup arg)
+- **Quasar**, **Helius Performance**, and [QuickNode Scale](https://www.quicknode.com/?via=rudearb) for sending transactions with spam feature
+- **Shyft gRPC** for the Yellowstone arg in jup api
+- [Rackdog 28 core Ashburn server](https://billing.rackdog.com/aff.php?aff=53)
+
+[Solscan Account](https://solscan.io/account/CboAt536xrx4MFq9rmV9n3v7YfLMPAom33VF8QRUpRBY#splTransfers)
+
+[Rackdog 28 core Ashburn server](https://billing.rackdog.com/aff.php?aff=53): $200  
+[QuickNode Scale Plan](https://www.quicknode.com/?via=rudearb): $300  
+Helius Performance Plan: $700  
+Quasar (gRPC and RPC): $500 (3 SOL)  
+Shyft gRPC: $200
+
+**Total Expenses: $1900 / month or ~$65 / day**
+
+You can play around with excluding/including some of these services but if you are subscribed to all of them then **your account needs to be profiting an average of ~$65 / day** to break even.
+
+And you need to be using up to at least 1k dollar trade sizes. Needless to say, an optimal setup is a big investment!
+
--- getTrendingTokens/main.py
+++ getTrendingTokens/main.py
@@ -1,58 +1,66 @@
 import requests
 import time
 
+base_token_strings = []
+
+for i in range(1, 11):
 # Make the GET request
-url = "https://api.geckoterminal.com/api/v2/networks/solana/trending_pools"
-response = requests.get(url)
+    url = f"https://api.geckoterminal.com/api/v2/networks/solana/trending_pools?page={i}"
+    print(url)
+    response = requests.get(url)
+    print(response)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON data
-    data = response.json()
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON data
+        data = response.json()
 
-    # Initialize an empty list to store the base token strings
-    base_token_strings = []
+        # Loop through the data and extract the base token strings
+        for pool in data["data"]:
+            base_token_id = pool["relationships"]["base_token"]["data"]["id"]
+            base_token_string = base_token_id.split("_")[1]
+            base_token_strings.append(base_token_string)
 
-    # Loop through the data and extract the base token strings
-    for pool in data["data"]:
-        base_token_id = pool["relationships"]["base_token"]["data"]["id"]
-        base_token_string = base_token_id.split("_")[1]
-        base_token_strings.append(base_token_string)
+        print(f"Gecko Iteration number {i}:")
+        # Print the base token strings
+        print(base_token_strings)
+        # print the length of the array
+        print(len(base_token_strings))
 
-    # Print the base token strings
-    print(base_token_strings)
-    # print the length of the array
-    print(len(base_token_strings))
-
-    print("Successfully fetched data from coingecko API")
-    
-    # Iterate over each base token string and make the GET request
-    jup_url = "https://quote-api.jup.ag/v6"
-    tokens_with_context_slot = []
-    for base_token in base_token_strings:
-        quote_url = f"{jup_url}/quote?inputMint=So11111111111111111111111111111111111111112&outputMint={base_token}&amount=1000000000&slippageBps=5&onlyDirectRoutes=false&asLegacyTransaction=false&maxAccounts=28"
-        quote_response = requests.get(quote_url)
-
-        if quote_response.status_code == 200:
-            response_json = quote_response.json()
-
-            # Check if the response has a contextSlot field
-            if "contextSlot" in response_json:
-                tokens_with_context_slot.append(base_token)
-                # has context slot so print the token
-                print(f"Valid SOL Pair token: {base_token}")
-
-            print("-" * 40)
-        else:
-            print(f"Invalid SOL Pair token {base_token}")
-
-        # Delay for 1 second
+        print("Successfully fetched data from coingecko API")
         time.sleep(1)
+    
 
-    # Print the tokens with contextSlot
-    print("Valid SOL Pair Tokens:")
-    print(tokens_with_context_slot)
-        
+# Iterate over each base token string and make the GET request
+jup_url = "https://quote-api.jup.ag/v6"
+tokens_with_context_slot = []
+for base_token in base_token_strings:
+    quote_url = f"{jup_url}/quote?inputMint=So11111111111111111111111111111111111111112&outputMint={base_token}&amount=1000000000&slippageBps=5&onlyDirectRoutes=false&asLegacyTransaction=false&maxAccounts=28"
+    quote_url = f"https://quote-api.jup.ag/v6/quote?inputMint=29BY5BHtLCG2mamRtC9hh9283U3QkbYLSCwXXmEJpump&outputMint=So11111111111111111111111111111111111111112&amount=1000000000&slippageBps=5&onlyDirectRoutes=false&asLegacyTransaction=false&maxAccounts=28"
 
-else:
-    print("Failed to fetch data from the API")
+    quote_response = requests.get(quote_url) # 29BY5BHtLCG2mamRtC9hh9283U3QkbYLSCwXXmEJpump
+
+    if quote_response.status_code == 200:
+        response_json = quote_response.json()
+
+        # Check if the response has a contextSlot field
+        if "contextSlot" in response_json:
+            tokens_with_context_slot.append(base_token)
+            # has context slot so print the token
+            print(f"Valid SOL Pair token: {base_token}")
+
+        print("-" * 40)
+    else:
+        print(f"INVALID SOL Pair token {base_token}")
+        print("-" * 40)
+
+    # Delay for 1 second
+    time.sleep(1)
+
+# Remove duplicates from tokens_with_context_slot
+tokens_with_context_slot = list(set(tokens_with_context_slot))
+# Print the tokens with contextSlot
+print("Valid SOL Pair Tokens:")
+print(tokens_with_context_slot)
+#length of the array
+print(len(tokens_with_context_slot))
